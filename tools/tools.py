13bb15b2327ff9eac7ce243f702980229e61ee98
Brian Barnes <houdinii1984@gmail.com>
2022-07-01 00:25:06 -0600

    Add utility to parse settings file and return universal Settings object

---
+++ tools/tools.py
@@ -0,0 +1,30 @@
+import json
+from box import Box
+
+
+def read_json(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("File not found: {}".format(file_path))
+        return None
+    except json.JSONDecodeError:
+        print("Invalid JSON: {}".format(file_path))
+        return None
+    except Exception as e:
+        print("{}".format(e))
+        return None
+
+
+class Settings:
+    def __init__(self, file_path):
+        self.config = self.get_settings(file_path)
+
+    def get_settings(self, file_path):
+        settings = read_json(file_path)
+        if settings is None:
+            print("No settings file found.")
+            raise FileNotFoundError
+        config = Box(settings)
+        return config
